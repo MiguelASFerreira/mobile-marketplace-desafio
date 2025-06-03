688eb516a04e96e1f89f8d9dc8e42df6dfd67334
MiguelASFerreira <migant1306@gmail.com>
2025-06-03 11:49:41 -0300

    Adds support for custom fonts with expo-font and updates dependencies

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { useFonts, DMSans_700Bold } from "@expo-google-fonts/dm-sans"
+import { Poppins_400Regular, Poppins_500Medium } from "@expo-google-fonts/poppins"
+
+
+
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    DMSans_700Bold,
+    Poppins_400Regular,
+    Poppins_500Medium
+  });
   return (
     <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      { fontsLoaded ? <Text>Open up App.tsx to start working on your app!</Text> : <Text>Loading...</Text> }
     </View>
   );
 }
