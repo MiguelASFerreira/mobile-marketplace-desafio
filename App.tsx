2835b7bb0570cab5ad11e85f2608d23c18255deb
MiguelASFerreira <migant1306@gmail.com>
2025-06-03 11:57:08 -0300

    Formats the code in the App.tsx component for better readability

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { useFonts, DMSans_700Bold } from "@expo-google-fonts/dm-sans"
-import { Poppins_400Regular, Poppins_500Medium } from "@expo-google-fonts/poppins"
-
-
-
-
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { useFonts, DMSans_700Bold } from "@expo-google-fonts/dm-sans";
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+} from "@expo-google-fonts/poppins";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     DMSans_700Bold,
     Poppins_400Regular,
-    Poppins_500Medium
+    Poppins_500Medium,
   });
   return (
     <View>
       <StatusBar style="auto" />
-      { fontsLoaded ? <Text>Open up App.tsx to start working on your app!</Text> : <Text>Loading...</Text> }
+      {fontsLoaded ? (
+        <Text>Open up App.tsx to start working on your app!</Text>
+      ) : (
+        <Text>Loading...</Text>
+      )}
     </View>
   );
 }
